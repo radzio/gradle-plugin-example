e0872a7577f1b12804e0a9719cc4e1d6f54fac49
Radek Piekarz <radoslaw.piekarz@gmail.com>
2017-08-30 16:00:21 +0200

    SendGrid tests

--- src/main/groovy/com/jhood/MyTask.groovy
+++ src/main/groovy/com/jhood/MyTask.groovy
@@ -1,5 +1,6 @@
 package com.jhood
 
+import com.sendgrid.*
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
@@ -11,5 +12,26 @@ class MyTask extends DefaultTask {
         outputFile.parentFile.mkdirs()
         outputFile.createNewFile()
         outputFile.text = project.extensions.myplugin.fileContent
+
+
+        Email from = new Email("androiddev@talixo.com");
+        String subject = "Sending with SendGrid is Fun";
+        Email to = new Email("piekarz@talixo.de");
+        Content content = new Content("text/plain", "and easy to do anywhere, even with Java");
+        Mail mail = new Mail(from, subject, to, content);
+
+        SendGrid sg = new SendGrid(roject.extensions.myplugin.fileContent);
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+        } catch (IOException ex) {
+            throw ex;
+        }
     }
 }
