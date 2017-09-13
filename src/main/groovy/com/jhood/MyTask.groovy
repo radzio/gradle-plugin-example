3c7bf5858d551a6f6fc8dd81532ea46a75a50353
Radek Piekarz <radoslaw.piekarz@gmail.com>
2017-09-13 10:30:43 +0200

    SendGrid tests #4

--- src/main/groovy/com/jhood/MyTask.groovy
+++ src/main/groovy/com/jhood/MyTask.groovy
@@ -10,22 +10,27 @@ class MyTask extends DefaultTask {
 
         Email from = new Email(project.extensions.myplugin.fromEmail);
         String subject = project.extensions.myplugin.subject;
-        Email to = new Email(project.extensions.myplugin.toEmail);
-        Content content = new Content("text/html", project.extensions.myplugin.content);
-        Mail mail = new Mail(from, subject, to, content);
 
-        SendGrid sg = new SendGrid(project.extensions.myplugin.sendgridApiKey);
-        Request request = new Request();
-        try {
-            request.setMethod(Method.POST);
-            request.setEndpoint("mail/send");
-            request.setBody(mail.build());
-            Response response = sg.api(request);
-            System.out.println(response.getStatusCode());
-            System.out.println(response.getBody());
-            System.out.println(response.getHeaders());
-        } catch (IOException ex) {
-            throw ex;
+        def emails = project.extensions.myplugin.toEmail.split(";")
+
+        for(email in emails) {
+            Email to = new Email(email);
+            Content content = new Content("text/html", project.extensions.myplugin.content);
+            Mail mail = new Mail(from, subject, to, content);
+
+            SendGrid sg = new SendGrid(project.extensions.myplugin.sendgridApiKey);
+            Request request = new Request();
+            try {
+                request.setMethod(Method.POST);
+                request.setEndpoint("mail/send");
+                request.setBody(mail.build());
+                Response response = sg.api(request);
+                System.out.println(response.getStatusCode());
+                System.out.println(response.getBody());
+                System.out.println(response.getHeaders());
+            } catch (IOException ex) {
+                throw ex;
+            }
         }
     }
 }
